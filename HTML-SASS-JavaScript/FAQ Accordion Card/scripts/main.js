622dccd2e1e56e8bab529872343c35ef7c003dbe
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-02-18 19:29:28 +0300

    Add files via upload

--- HTML-SASS-JavaScript/FAQ Accordion Card/scripts/main.js
+++ HTML-SASS-JavaScript/FAQ Accordion Card/scripts/main.js
@@ -3,12 +3,12 @@ const boldFontWeight = 700;
 
 function onClick(btn) {
   let fontWeight = window.getComputedStyle(btn).getPropertyValue("font-weight");
-  let img = btn.children[1];
+  let img = btn.children[0];
 
   if (fontWeight == regularFontWeight) {
     btn.style.fontWeight = boldFontWeight;
     img.style.transform = "rotate(180deg)";
-    btn.nextElementSibling.style.display = "inline-block";
+    btn.nextElementSibling.style.display = "block";
   } else {
     btn.style.fontWeight = regularFontWeight;
     img.style.transform = "none";
