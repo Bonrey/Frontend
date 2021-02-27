58ce3e832f404eec6955b52258411406a50d6c6c
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-02-27 09:35:25 +0300

    Add files via upload

--- HTML-SASS-JavaScript/Social Media Dashboard/js/main.js
+++ HTML-SASS-JavaScript/Social Media Dashboard/js/main.js
@@ -8,8 +8,27 @@ for (let i = 0, len = focusableElements.length; i < len; i++) {
   focusableElements[i].setAttribute("tabindex", "0");
 }
 
-// toggle theme
-toggle.addEventListener("change", () => body.className = toggle.checked ? "dark-theme" : "light-theme");
+// toggle theme + cards transition animation
+const cards = document.querySelectorAll(".top-card section, .overview-section__card");
+toggle.addEventListener("change", () => {
+  const len = cards.length;
+  if (toggle.checked) {
+    body.className = "dark-theme";
+    for (let i = 0; i < len; i++) {
+      cards[i].style.transition = (400 + (3 - i % 4) * 200).toString() + "ms";
+    }
+  } else {
+    body.className = "light-theme";
+    for (let i = 0; i < len; i++) {
+      cards[i].style.transition = (400 + (i % 4) * 200).toString() + "ms";
+    }
+  }
+  window.setTimeout(() => {
+    for (let i = 0, len = cards.length; i < len; i++) {
+      cards[i].style.transition = "0.3s";
+    }
+  }, 1000);
+});
 
 // show a horizontal line on smaller screens
 const onResize = () => divider.style.display = window.outerWidth > 640 ? "none" : "block";
