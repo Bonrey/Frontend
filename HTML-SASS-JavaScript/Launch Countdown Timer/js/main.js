d5130fa1057d1a10a35f23426cc98afdd216c10c
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-05 14:22:19 +0300

    Add files via upload

--- HTML-SASS-JavaScript/Launch Countdown Timer/js/main.js
+++ HTML-SASS-JavaScript/Launch Countdown Timer/js/main.js
@@ -1,9 +1,10 @@
 const circles = document.querySelectorAll("svg circle");
 
+// correctly resize svg circles depending on the screen size
 function correctCircleRadius() {
   if (window.outerWidth <= 720) {
     for (let i = 0, len = circles.length; i < len; i++) {
-      circles[i].style.r = "3px";
+      circles[i].style.r = "0.9vw";
     }
   } else {
     for (let i = 0, len = circles.length; i < len; i++) {
@@ -14,23 +15,6 @@ function correctCircleRadius() {
 
 window.addEventListener("resize", correctCircleRadius);
 correctCircleRadius();
-
-
-// let countDownDate = new Date("Mar 6, 2021 2:18:52").getTime();
-// const dateCards = document.querySelectorAll("#days, #hours, #minutes, #seconds");
-// const timeLeftUpdate = setInterval(() => {
-//   let timeLeft = countDownDate - new Date().getTime();
-//   let timeUnits = [
-//     Math.floor(timeLeft / (1000 * 60 * 60 * 24)),  // days
-//     Math.floor(timeLeft % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),  // hours
-//     Math.floor(timeLeft % (1000 * 60 * 60) / (1000 * 60)),  // minutes
-//     Math.floor((timeLeft % (1000 * 60)) / 1000)  // seconds
-//   ];
-//
-//   for (let i = 0; i < 4; i++) {
-//     dateCards[i].innerHTML = ("0" + timeUnits[i]).slice(-2);
-//   }
-// }, 1000);
 
 
 let cards = {
@@ -60,14 +44,12 @@ let cards = {
   }
 };
 
-// let timeUnits = [
-//   Number.parseInt(cards.days.hiddenNumber.innerText),     // days
-//   Number.parseInt(cards.hours.hiddenNumber.innerText),    // hours
-//   Number.parseInt(cards.minutes.hiddenNumber.innerText),  // minutes
-//   Number.parseInt(cards.seconds.hiddenNumber.innerText)   // seconds
-// ];
-
-let timeUnits = [1, 1, 0, 5];
+let timeUnits = [
+  Number.parseInt(cards.days.hiddenNumber.innerText),     // days
+  Number.parseInt(cards.hours.hiddenNumber.innerText),    // hours
+  Number.parseInt(cards.minutes.hiddenNumber.innerText),  // minutes
+  Number.parseInt(cards.seconds.hiddenNumber.innerText)   // seconds
+];
 
 function flipCard(card, val) {
   card.hiddenNumber.innerText = ("0" + val).slice(-2);
@@ -93,21 +75,47 @@ function restoreDefault(card, val) {
   card.bottomNumber.style.color = "hsl(345, 95%, 68%)";
 }
 
-let t = false;
-const flipInterval = setInterval(() => {
-  if (!t) {
-    t = true;
+
+let finished = false;
+const heading = document.getElementsByTagName("h1")[0];
+const fini = () => {
+  finished = true;
+  // setInterval() will still work once (one last iteration that has already started)
+  clearInterval(flipInterval);
+  setTimeout(() => {
+    let i = 0, endMessage = ["F", "I", "N", "I"];
+    for (let card in cards) {
+      cards[card].topNumber.style.transition = "opacity 2s";
+      cards[card].topNumber.style.opacity = "0";
+
+      cards[card].bottomNumber.style.transition = "opacity 2s";
+      cards[card].bottomNumber.style.opacity = "0";
+
+      heading.style.transition = "opacity 2s";
+      heading.style.opacity = "0";
+
+      setTimeout(() => {
+        cards[card].topNumber.innerText = endMessage[i];
+        cards[card].bottomNumber.innerText = endMessage[i++];
+        cards[card].topNumber.style.opacity = "1";
+        cards[card].bottomNumber.style.opacity = "1";
+        heading.innerText = "We have launched!";
+        heading.style.opacity = "1";
+      }, 2000);
+    }
+  }, 1000);
+}
+
+function update(timeSkipped = false) {
+  if (timeUnits[0] === 0 && timeUnits[1] === 0 && timeUnits[2] === 0 && timeUnits[3] === 1) {
+    fini();
+  }
+  if (timeSkipped) {
     let i = 0;
     for (let card in cards) {
       flipCard(cards[card], timeUnits[i++]);
     }
   } else {
-    if (timeUnits[0] === 0 && timeUnits[1] === 0 && timeUnits[2] === 0 && timeUnits[3] === 1) {
-      clearInterval(flipInterval);  // setInterval() will still work once (till the end of this block)
-      setTimeout(() => {
-        alert("End!");
-      }, 2000);
-    }
     timeUnits[3] = (timeUnits[3] + 59) % 60;
     flipCard(cards.seconds, timeUnits[3]);
     if (timeUnits[3] === 59) {
@@ -137,4 +145,54 @@ const flipInterval = setInterval(() => {
       restoreDefault(cards[card], timeUnits[i++]);
     }
   }, 900);
-}, 1000);
+}
+
+let flipInterval = setInterval(update, 1000);
+
+// 1) pause/resume the countdown timer
+// 2) see what happens at the end of the countdown!
+let paused = false, enterTimeout = -1;
+document.addEventListener("keydown", (e) => {
+  if (e.key === " " || e.key === "Enter") {
+    clearTimeout(enterTimeout);
+    clearInterval(flipInterval);
+  }
+
+  if (e.key === " " && !finished) {
+    if (paused) {
+      flipInterval = setInterval(update, 1000);
+    }
+    paused = !paused;
+  } else if (e.key === "Enter") {
+    enterTimeout = setTimeout(() => {
+      timeUnits = [0, 0, 0, 4];
+      update(true);
+      heading.style.transition = "opacity 500ms";
+      heading.style.opacity = "0";
+      setTimeout(() => {
+        heading.style.opacity = "1";
+        heading.innerText = "We're launching soon";
+      }, 500);
+      flipInterval = setInterval(update, 1000);
+    }, finished ? 4000 : 1000);
+    paused = finished = false;
+  }
+});
+
+// social media links will trigger page refresh
+let socialMediaLinks = document.querySelectorAll(".social-media a");
+for (let i = 0, len = socialMediaLinks.length; i < len; i++) {
+  socialMediaLinks[i].addEventListener("click", () => {
+    location.reload();
+  });
+}
+
+// AUDIO AUTOPLAY IS NOT ALLOWED (FOR SOME REASON) !
+// let promise = document.getElementById("ticking").play();
+// if (promise !== undefined) {
+//   promise.then(() => {
+//     console.log("autoplay started!");
+//   }).catch(error => {
+//     console.log("autoplay was prevented!");
+//   });
+// }
