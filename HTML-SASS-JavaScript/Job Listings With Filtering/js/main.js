93b3e257dd44163f9083f65b14574831141225e4
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-07 21:17:35 +0300

    js added

---
+++ HTML-SASS-JavaScript/Job Listings With Filtering/js/main.js
@@ -0,0 +1,129 @@
+const filtersMenu = document.getElementsByClassName("filters-menu")[0];
+const itemsWrapper = document.getElementsByClassName("items-wrapper")[0];
+
+const items = {
+  "photosnap-item": ["frontend", "senior", "html", "css", "javascript"],
+  "manage-item": ["fullstack", "midweight", "python", "react"],
+  "account-item": ["frontend", "junior", "javascript", "react", "sass"],
+  "myhome-item": ["frontend", "junior", "css", "javascript"],
+  "loop-studios-item": ["fullstack", "midweight", "javascript", "ruby", "sass"],
+  "faceit-item": ["backend", "junior", "ruby", "ror"],
+  "shortly-item": ["frontend", "junior", "html", "javascript", "sass"],
+  "insure-item": ["frontend", "junior", "javascript", "vue", "sass"],
+  "eyecam-item": ["fullstack", "midweight", "javascript", "python", "django"],
+  "air-filter-item": ["frontend", "junior", "javascript", "react", "sass"]
+};
+
+let itemElements = {};
+for (let key in items) {
+  itemElements[key] = document.getElementById(key);
+}
+let filters = [];
+
+
+// ===================
+// update upper margin
+// ===================
+let filtersMenuShown = false;
+const updateTopMargin = () => itemsWrapper.style.marginTop = filtersMenuShown ? "21px" : "50px";
+
+
+// ======================
+// hide/show filters menu
+// ======================
+function hideFiltersMenu() {
+  filtersMenu.style.opacity = "0";
+  setTimeout(() => {
+    filtersMenu.style.display = "none";
+    updateTopMargin();
+  }, 300);
+  filtersMenuShown = false;
+}
+
+function showFiltersMenu() {
+  filtersMenu.style.display = "flex";
+  filtersMenu.style.animation = "visible 2s 1";
+  setTimeout(() => {
+    filtersMenu.style.opacity = "1";
+  }, 300);
+  filtersMenuShown = true;
+  updateTopMargin();
+}
+
+
+// ==============================
+// remove buttons for each filter
+// ==============================
+const removeFilterButtons = document.querySelectorAll(".filters-menu__filters button");
+for (let i = 0, len = removeFilterButtons.length; i < len; i++) {
+  removeFilterButtons[i].addEventListener("click", () => {
+    let filterElement = removeFilterButtons[i].parentElement;
+    filterElement.style.opacity = "0";
+    setTimeout(() => {
+      filterElement.style.display = "none";
+    }, 300);
+
+    filters.splice(filters.indexOf(filterElement.id), 1);
+    updateItems();
+    if (filters.length === 0) {
+      hideFiltersMenu();
+    }
+  });
+}
+
+
+// ================
+// clear-all button
+// ================
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+  for (let i = 0, len = filters.length; i < len; i++) {
+    let filterElement = document.getElementById(filters[i]);
+    filterElement.style.opacity = "0";
+    setTimeout(() => {
+      filterElement.style.display = "none";
+    }, 300);
+  }
+  filters = [];
+  updateItems();
+  hideFiltersMenu();
+});
+
+
+// ===============
+// setting filters
+// ===============
+const filterTablets = document.querySelectorAll(".item .filters button");
+for (let i = 0, len = filterTablets.length; i < len; i++) {
+  filterTablets[i].addEventListener("click", () => {
+    if (!filtersMenuShown) showFiltersMenu();
+
+    let filterElement = document.getElementById(filterTablets[i].innerText.toLowerCase());
+    if (filters.indexOf(filterElement.id) === -1) {
+      filterElement.style.display = "flex";
+      filterElement.style.animation = "visible 2s 1";
+      setTimeout(() => {
+        filterElement.style.opacity = "1";
+      }, 300);
+      filters.push(filterElement.id);
+      updateItems();
+    }
+  });
+}
+
+
+// ==================================
+// leaving only the appropriate items
+// ==================================
+function updateItems() {
+  for (let item in items) {
+    let show = true;
+    for (let i = 0, len = filters.length; i < len; i++) {
+      if (items[item].indexOf(filters[i]) === -1) {
+        show = false;
+        break
+      }
+    }
+    itemElements[item].style.display = show ? "flex" : "none";
+  }
+}
