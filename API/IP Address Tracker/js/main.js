1aee1a870be0168e243fd6a3b1d73913b1761c1f
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-10 23:00:53 +0300

    some improvements
    added search by domain

--- API/IP Address Tracker/js/main.js
+++ API/IP Address Tracker/js/main.js
@@ -1,4 +1,4 @@
-const apiKey = "at_EydNviLJCZNMWuRaWCSJtngeQFCUJ";
+const apiKey = "at_yD616QsY5LwKtmSTbfr3fiwpFDfDI"; //"at_EydNviLJCZNMWuRaWCSJtngeQFCUJ";
 const dataElements = {
   "ip": document.querySelector("#ip-address span"),
   "location": document.querySelector("#location span"),
@@ -15,9 +15,6 @@ const loaders = {
 
 
 let map = L.map("map", {zoomControl: false});
-let customMarker = L.icon({
-  iconUrl: "images/icon-location.svg"
-});
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -27,48 +24,83 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(map);
 
 
+// =======================
+// handle inaccurate input
+// =======================
+function invalidInput() {
+  textField.placeholder = "Enter correct IP address or domain!";
+  textField.classList.add("red-placeholder");
+
+  for (let dataElement in dataElements) {
+    dataElements[dataElement].innerText = "";
+  }
+  for (let loader in loaders) {
+    loaders[loader].style.display = "none";
+  }
+}
+
+
 // ===================
 // update data and map
 // ===================
-function getGeoInfo(firstLaunch = false, ipInput, domainInput) {
-  fetch("https://api.ipify.org/?format=json")
+function getGeoInfo(firstLaunch, ipInput = "", domainInput = "") {
+  fetch("https://api.ipify.org?format=json")
     .then(response => response.json())
     .then(data => {
       if (firstLaunch) {
         loaders["ip-loader"].style.display = "none";
         dataElements.ip.innerText = data.ip;
+        return ["ip", data.ip];
       } else {
         if (ipInput) {
-          dataElements.ip.innerText = ipInput;
-          return ipInput;
+          return ["ip", ipInput];
         } else if (domainInput) {
-
-        } else {
-          console.log("Error: incorrect IP or domain!");
+          return ["domain", domainInput];
         }
+        throw "invalid input!";
       }
-      return ["ip", dataElements.ip.innerText];
     })
     .then(data => {
-      let requestUrl = "https://geo.ipify.org/api/v1?";
-      fetch(`${requestUrl}apiKey=${apiKey}&ipAddress=${ip}`)
+      let requestUrl = `https://geo.ipify.org/api/v1?apiKey=${apiKey}`;
+      if (data[0] === "ip") {
+        requestUrl += `&ipAddress=${data[1]}`;
+      } else if (data[0] === "domain") {
+        requestUrl += `&domain=${data[1]}`;
+      }
+      return fetch(requestUrl);
     })
-    .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      throw "bad url!";
+    })
     .then(data => {
-      loaders["location-loader"].style.display = "none";
-      loaders["timezone-loader"].style.display = "none";
-      loaders["isp-loader"].style.display = "none";
+        loaders["ip-loader"].style.display = "none";
+        loaders["location-loader"].style.display = "none";
+        loaders["timezone-loader"].style.display = "none";
+        loaders["isp-loader"].style.display = "none";
 
-      dataElements.location.innerText = `${data.location.region}, ${data.location.country}`;
-      dataElements.timezone.innerText = "UTC " + data.location.timezone;
-      dataElements.isp.innerText = data.isp;
+        console.log("even here");
+        dataElements["ip"].innerText = data.ip;
+        dataElements.location.innerText = `${data.location.region}, ${data.location.country}`;
+        dataElements.timezone.innerText = "UTC " + data.location.timezone;
+        dataElements.isp.innerText = data.isp;
 
-      const {lat, lng} = data.location;
-      map.setView([lat, lng], 12);
+        const {lat, lng} = data.location;
+        map.setView([lat, lng], 16);
 
-      L.marker([lat, lng], {icon: customMarker})
-        .bindPopup(`<b>${data.location.region}, ${data.location.country}</b>`)
-        .addTo(map);
+        L.marker([lat, lng], {
+          icon: L.icon({
+            iconUrl: "images/icon-location.svg",
+            iconAnchor: [lat, lng]
+          })
+        }).bindPopup(`<b>${data.location.region}, ${data.location.country}</b>`).addTo(map);
+      }
+    )
+    .catch(error => {
+      console.log(error);
+      invalidInput();
     });
 }
 
@@ -81,35 +113,34 @@ getGeoInfo(true);
 // ============================
 const textField = document.querySelector("input[type='text']");
 
-function isIpValid(ip) {
+function isValidIp(ip) {
   const ipNum = "([0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
   const reg = new RegExp(`^${ipNum}\\.${ipNum}\\.${ipNum}\\.${ipNum}$`);
   return reg.test(ip);
 }
 
-function invalidIpError() {
-  console.log("invalid ip");
-}
 
+// ==========================
+// handle search button click
+// ==========================
 function updateGeoInfo() {
-  let ip = textField.value;
+  const input = textField.value;
   textField.value = "";
 
-  if (!isIpValid(ip)) {
-    textField.placeholder = "Enter correct IP address or domain!";
-    textField.classList.add("red-placeholder");
-    invalidIpError();
+  dataElements.location.innerText = "";
+  dataElements.timezone.innerText = "";
+  dataElements.isp.innerText = "";
+
+  loaders["location-loader"].style.display = "block";
+  loaders["timezone-loader"].style.display = "block";
+  loaders["isp-loader"].style.display = "block";
+
+  if (isValidIp(input)) {
+    loaders["ip-loader"].style.display = "none";
+    dataElements.ip.innerText = input;
+    getGeoInfo(false, input);
   } else {
-    dataElements.ip.innerText = ip;
-    dataElements.location.innerText = "";
-    dataElements.timezone.innerText = "";
-    dataElements.isp.innerText = "";
-
-    loaders["location-loader"].style.display = "block";
-    loaders["timezone-loader"].style.display = "block";
-    loaders["isp-loader"].style.display = "block";
-
-    getGeoInfo(ip);
+    getGeoInfo(false, "", input);
   }
 }
 
@@ -117,6 +148,9 @@ const searchButton = document.querySelector("button[type='submit']");
 searchButton.addEventListener("click", updateGeoInfo);
 
 
+// ================================================================
+// bring the text field to the default state after clicking outside
+// ================================================================
 const form = document.getElementsByTagName("form")[0];
 document.addEventListener("click", e => {
   if (!form.contains(e.target) && textField.classList.contains("red-placeholder")) {
