1778d4c6bf7c2a49299395b6f98619a5bb9354d0
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-18 14:02:17 +0300

    Add files via upload

--- HTML-SASS-JavaScript/Manage Landing Page/js/main.js
+++ HTML-SASS-JavaScript/Manage Landing Page/js/main.js
@@ -1,6 +1,7 @@
 // ==============
 // ____HEADER____
 // ==============
+const btnSound = new Audio("media/button-click.ogg");
 const pageMask = document.getElementById("page-mask");
 const headerMenu = document.getElementById("header-menu");
 const menuButton = document.querySelector("button.menu-btn");
@@ -8,9 +9,16 @@ const menuButtonImage = menuButton.firstElementChild;
 
 let menuOpened = false;
 menuButton.addEventListener("click", _ => {
+  btnSound.play();
+  // to prevent the user from clicking too much
+  menuButton.style.pointerEvents = "none";
+  setTimeout(_ => menuButton.style.pointerEvents = "auto", 1000);
+  // some animation and display changes
   menuButtonImage.src = menuOpened ? "images/icon-hamburger.svg" : "images/icon-close.svg";
-  pageMask.style.animation = headerMenu.style.animation = menuOpened ? "hide 1s 1" : "show 1s 1";
+  pageMask.style.animation = headerMenu.style.animation = menuOpened ? "hide 1050ms 1" : "show 1s 1";
   menuButtonImage.style.animation = menuOpened ? "rotate-clockwise 1s 1" : "rotate-counterclockwise 1s 1";
+  setTimeout(_ => pageMask.style.animation = headerMenu.style.animation = menuButtonImage.style.animation = "none", 1000);
+  // settings after animation is completed
   setTimeout(_ => {
     pageMask.style.display = menuOpened ? "none" : "block";
     headerMenu.style.display = menuOpened ? "none" : "flex";
@@ -28,6 +36,7 @@ for (let i = 0, offset = -88; i < allSlides.length; i++, offset += 35.5) {
   allSlides[i].style.left = `calc(50% + ${offset}rem)`;
 }
 
+const dotClickAudio = new Audio("media/testimonials-click.wav");
 const dots = document.querySelectorAll(".dot");
 let [translate, current] = [0, 1];
 
@@ -46,6 +55,7 @@ function animateSlides() {
 
 for (let i = 0; i < dots.length; i++) {
   dots[i].addEventListener("click", () => {
+    dotClickAudio.play();
     colorDot(i);
     translate += (current - i) * 35.5;
     animateSlides();
@@ -63,7 +73,7 @@ function autoUpdateFunc() {
         allSlides[i].style.transition = "none";
         allSlides[i].style.transform = "translateX(35.5rem)";
       }
-      // without this timeout, the above translateX would execute WITH transition of 1s
+      // without this timeout, the above translateX would execute WITH transition of 2s
       setTimeout(() => {
         for (let i = 0; i < allSlides.length; i++) {
           allSlides[i].style.transition = "transform 2s";
@@ -76,19 +86,20 @@ function autoUpdateFunc() {
   colorDot(current);
 }
 
-let autoUpdate = setInterval(autoUpdateFunc, 4000);
+let autoUpdate = setInterval(autoUpdateFunc, 5000);
 const testimonialsSlider = document.querySelector(".testimonials-slider");
 testimonialsSlider.addEventListener("mouseenter", () => {
   clearInterval(autoUpdate);
 });
 testimonialsSlider.addEventListener("mouseleave", () => {
-  autoUpdate = setInterval(autoUpdateFunc, 4000);
+  autoUpdate = setInterval(autoUpdateFunc, 5000);
 });
 
 
 // ==============
 // ____FOOTER____
 // ==============
+const errorSound = new Audio("media/error.flac");
 const form = document.querySelector(".subscription-form form");
 const submitButton = document.querySelector("input[type=submit]");
 const textField = document.querySelector("input[type=text]");
@@ -99,19 +110,24 @@ form.addEventListener("submit", e => e.preventDefault());
 function defaultInputField() {
   textField.placeholder = "Updates in your inbox…";
   textField.classList.replace("error-text", "default-text");
-  textFieldErrorLabel.style.visibility = "hidden";
+  textFieldErrorLabel.style.opacity = "0";
 }
 
 function errorInputField() {
   textField.value = "";
   textField.placeholder = "example@mail.ru";
   textField.classList.replace("default-text", "error-text");
-  textFieldErrorLabel.style.visibility = "visible";
+  textFieldErrorLabel.style.opacity = "1";
+  textField.style.animation = "shake 0.4s 1";
+  setTimeout(_ => textField.style.animation = "none", 400);
 }
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", _ => {
   if (!/\w+@\w+\.\w+/.test(textField.value)) {
+    errorSound.play();
     errorInputField();
+  } else {
+    btnSound.play();
   }
 });
 
@@ -126,9 +142,58 @@ document.addEventListener("click", e => {
 });
 
 
-// ======================
-// ___ON WINDOW RESIZE___
-// ======================
+// ===================
+// ____ATTRIBUTION____
+// ===================
+const attrButton = document.getElementById("attribution-image");
+const attrText = document.querySelector(".attribution-text");
+const attrSound = new Audio("media/attr-click.wav");
+attrSound.volume = 0.5;
+let attrShown = false;
+
+attrButton.addEventListener("click", _ => {
+  attrSound.play();
+  attrButton.style.pointerEvents = "none";
+  setTimeout(_ => attrButton.style.pointerEvents = "auto", 1000);
+  if (attrShown) {
+    attrText.style.animation = "hide-attr 1s 1";
+    setTimeout(_ => attrText.style.display = "none", 950);
+  } else {
+    attrText.style.animation = "show-attr 1s 1";
+    attrText.style.display = "block";
+  }
+  setTimeout(_ => attrText.style.animation = "none", 1000);
+  attrShown = !attrShown;
+});
+
+attrButton.addEventListener("mouseenter", _ => attrButton.style.opacity = "1");
+attrButton.addEventListener("mouseleave", _ => attrButton.style.opacity = attrShown ? "1" : "0.5");
+
+
+// ==================
+// ___OTHER SOUNDS___
+// ==================
+const elements = document.querySelectorAll(".social-media__links a, .get-started-btn");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", _ => {
+    btnSound.play();
+  });
+}
+
+
+// ========================
+// __FIRST TIME ANIMATION__
+// ========================
+if (outerWidth > 720) {
+  document.querySelector("header").style.animation = "show-header 2s ease-out 1";
+  document.querySelector(".intro-text").style.animation = "intro-text-show 2s ease-out 1";
+  document.querySelector(".intro-img").style.animation = "intro-image-show 2s ease-out 1";
+}
+
+
+// ====================
+// __ON WINDOW RESIZE__
+// ====================
 function onWindowResize() {
   if (outerWidth <= 720) {
     headerMenu.style.display = menuOpened ? "flex" : "none";
@@ -144,4 +209,4 @@ function onWindowResize() {
 }
 
 addEventListener("resize", onWindowResize);
-onWindowResize();
+onWindowResize();  // to set the correct styles for a mobile/desktop
