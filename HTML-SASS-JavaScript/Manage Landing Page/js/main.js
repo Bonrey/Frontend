36596c05fea60ee1e4e3a53186b793425b8664c7
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-18 22:31:18 +0300

    Update main.js

--- HTML-SASS-JavaScript/Manage Landing Page/js/main.js
+++ HTML-SASS-JavaScript/Manage Landing Page/js/main.js
@@ -195,19 +195,19 @@ if (outerWidth > 720) {
 // ====================
 // __ON WINDOW RESIZE__
 // ====================
-function onWindowResize() {
-  if (outerWidth <= 720) {
-    headerMenu.style.display = menuOpened ? "flex" : "none";
-    menuButton.style.display = "flex";
-  } else {
-    headerMenu.style.display = "block";
-    pageMask.style.display = menuButton.style.display = "none";
-    headerMenu.style.animation = menuButtonImage.style.animation = "none";
-    menuButtonImage.src = "images/icon-hamburger.svg";
-    headerMenu.classList.remove("menu-mobile");
-    menuOpened = false;
-  }
-}
+// function onWindowResize() {
+//   if (outerWidth <= 720) {
+//     headerMenu.style.display = menuOpened ? "flex" : "none";
+//     menuButton.style.display = "flex";
+//   } else {
+//     headerMenu.style.display = "block";
+//     pageMask.style.display = menuButton.style.display = "none";
+//     headerMenu.style.animation = menuButtonImage.style.animation = "none";
+//     menuButtonImage.src = "images/icon-hamburger.svg";
+//     headerMenu.classList.remove("menu-mobile");
+//     menuOpened = false;
+//   }
+// }
 
-addEventListener("resize", onWindowResize);
-onWindowResize();  // to set the correct styles for a mobile/desktop
+// addEventListener("resize", onWindowResize);
+// onWindowResize();  // to set the correct styles for a mobile/desktop
