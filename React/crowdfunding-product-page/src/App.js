6c96685ddebcf3f1c1848afbcf327b1085e342b8
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-26 19:40:04 +0300

    Add files via upload

--- React/crowdfunding-product-page/src/App.js
+++ React/crowdfunding-product-page/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Main from './components/Main';
+import Attribution from './components/Attribution';
 
 import './App.scss';
 
-export default function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popupVisible: false
+    };
+  }
+
+  handleClick(popupVisible) {
+    this.setState({ popupVisible: popupVisible });
+  }
+
+  render() {
+    return (
+      <div className={this.state.popupVisible ? "darkened" : ""}>
+        <Header />
+        <Main
+          popupVisible={this.state.popupVisible}
+          onClick={param => this.handleClick(param)}
+        />
+        {/*<Attribution />*/}
+      </div>
+    );
+  }
 }
