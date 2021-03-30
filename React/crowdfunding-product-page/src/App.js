e3f2ce94b3ba25fc23675f0ade95673cb6dcece2
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-03-30 21:53:00 +0300

    Add files via upload

--- React/crowdfunding-product-page/src/App.js
+++ React/crowdfunding-product-page/src/App.js
@@ -1,32 +1,49 @@
 import React from 'react';
 
+import Preloaded from './components/Preloaded';
 import Header from './components/Header';
 import Main from './components/Main';
 //import Attribution from './components/Attribution';
 
-import './App.scss';
+import './css/App.min.css';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      popupVisible: false
+      preloaded: false,
+      btnClicked: false,
+      popupVisible: false,
+      popupFadeIn: false
     };
   }
 
+  componentDidMount() {
+    this.setState({ preloaded: true });
+  }
+
   handleClick(popupVisible) {
-    this.setState({ popupVisible: popupVisible });
+    this.setState({ btnClicked: true, popupFadeIn: popupVisible });
+    setTimeout(_ => this.setState({
+      popupVisible: popupVisible
+    }), popupVisible ? 0 : 400);
   }
 
   render() {
     return (
-      <div className={this.state.popupVisible ? "darkened" : ""}>
-        <Header />
-        <Main
-          popupVisible={this.state.popupVisible}
-          onClick={param => this.handleClick(param)}
-        />
-        {/*<Attribution />*/}
+      <div>
+        {!this.state.preloaded && <Preloaded />}
+        {this.state.preloaded &&
+        <div className={!this.state.btnClicked ? "" : this.state.popupFadeIn ? "dark-bg fade-in" : "dark-bg fade-out"}>
+          <Header />
+          <Main
+            className={!this.state.btnClicked ? "" : this.state.popupFadeIn ? " show-popup" : " hide-popup"}
+            popupVisible={this.state.popupVisible}
+            onClick={param => this.handleClick(param)}
+          />
+          {/*<Attribution />*/}
+        </div>
+        }
       </div>
     );
   }
